698c1d1efdcfae938625e56a80d913bbb1efe450
jamesveug <james.veugelaers@outlook.co.nz>
2021-12-11 20:56:53 +1300

    Band-Aid Fixed MergeSequence breaking due to a missing script

---
+++ Mod/Patches/DuplicateMergeSequency_Patches.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using DiskCardGame;
+using HarmonyLib;
+using UnityEngine;
+
+namespace MaskMod.Patches
+{
+    [HarmonyPatch(typeof (DuplicateMergeSequencer), "SetSideHeadTalking", new System.Type[] {(typeof(bool))})]
+    public class DuplicateMergeSequencer_SetSideHeadTalking
+    {
+        public static bool Prefix(bool sideHeadTalking)
+        {
+            // TODO: Make custom masks work with the side head rotating thingy
+            // Requires: DoctorMask on the mask and a set of eyes ot turn on and off
+            return false;
+        }
+    }
+}
